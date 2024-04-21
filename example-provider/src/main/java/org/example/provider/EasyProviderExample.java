fda22d0352e7cabc9e5ff434f37609c1cf250cca
ZMX321 <71180480+ZMX321@users.noreply.github.com>
2024-04-21 10:09:47 -0700

    add config usage on provider

--- example-provider/src/main/java/org/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/org/example/provider/EasyProviderExample.java
@@ -2,6 +2,7 @@ package org.example.provider;
 
 
 import org.example.common.service.UserService;
+import org.example.rpc.RpcApplication;
 import org.example.rpc.easy.registry.LocalRegistry;
 import org.example.rpc.easy.server.HttpServer;
 import org.example.rpc.easy.server.VertxHttpServer;
@@ -9,9 +10,10 @@ import org.example.rpc.easy.server.VertxHttpServer;
 public class EasyProviderExample {
 
     public static void main(String[] args) {
+        RpcApplication.init();
         LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
 
         HttpServer httpServer = new VertxHttpServer();
-        httpServer.doStart(8080);
+        httpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
     }
 }
